318b0a88e60789e64cf195ea41aa59acae231931
Jerbeson Vinycius <jerbesonvinycius@gmail.com>
2021-10-13 15:47:03 -0300

    Criada a função que permite obter eventos baseados em um usuário

--- app/controllers/eventcontroller.py
+++ app/controllers/eventcontroller.py
@@ -21,3 +21,13 @@ async def get_all_events() -> list:
     ) # Selecionando todos os dados da tabela de eventos
 
     return query.scalars().all()
+
+# Função que permite captar os eventos cadastrados por um usuário específico
+async def get_events_per_user(user_uuid: str) -> list:
+  # Abrindo uma sessão no banco
+  async with Session() as s:
+    query = await s.execute(
+      select(Event).where(Event.author_uuid == user_uuid)
+    ) # Selecionando todos os eventos que tenham sido criados pelo usuário informado
+
+    return query.scalars().all()
